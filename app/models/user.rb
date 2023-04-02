fc86e2f4189d2d3be6cbf5a952dedfec764dec8d
Taku Fukaura <terekusaitama@gmail.com>
2023-04-02 16:27:48 +0900

    [Fix] ユーザー情報にHobbyとProfileを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,8 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :profile, presence: false, length: { maximum: 200 }
+  validates :hobby, presence: false, length: { maximum: 100 }
 
   validates :email, uniqueness: true
 
