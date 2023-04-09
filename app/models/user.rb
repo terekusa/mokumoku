269685280259efd01de4a5651f86dfbf63bbd6f9
Taku Fukaura <terekusaitama@gmail.com>
2023-04-09 18:00:46 +0900

    [Fix] Userモデルのフォロー関係の関連付け、profile/hobbyのバリデーション、unfollowメソッドの記述を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,16 @@ class User < ApplicationRecord
   has_many :user_notification_timings, dependent: :destroy
   has_many :notification_timings, through: :user_notification_timings
   has_one_attached :avatar
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following_user, through: :follower, source: :followed
-  has_many :follower_user, through: :followed, source: :follower
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_users, through: :follower_relationships, source: :followed
+  has_many :follower_user, through: :followed_relationships, source: :follower
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :profile, presence: false, length: { maximum: 200 }
-  validates :hobby, presence: false, length: { maximum: 100 }
+  validates :profile, length: { maximum: 200 }
+  validates :hobby, length: { maximum: 100 }
 
   validates :email, uniqueness: true
 
@@ -85,16 +85,16 @@ class User < ApplicationRecord
 
   # ユーザーをフォローする
   def follow(user_id)
-    follower.create(followed_id: user_id)
+    follower_relationships.create(followed_id: user_id)
   end
 
   # ユーザーのフォローを外す
   def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
+    !!follower_relationships.find_by(followed_id: user_id)&.destroy
   end
 
   # フォローしていればtrueを返す
   def following?(user)
-    following_user.include?(user)
+    following_users.include?(user)
   end
 end
